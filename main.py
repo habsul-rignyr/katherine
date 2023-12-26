9c93cad24b00ac36bbc793191f83b4b5d0fb5f35
habsul-rignyr <joshamy@gmail.com>
2023-12-26 15:32:03 -0600

    Integrate gpt3

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+import discord
+from openai import OpenAI
+from config import DISCORD_TOKEN, OPENAI_API_KEY
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# load OpenAI key
+openai_client = OpenAI(
+    api_key = OPENAI_API_KEY
+)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# declare intents
+intents = discord.Intents.default()
+intents.messages = True
+intents.guilds = True
+intents.message_content = True
+
+# Initialize Discord client with specified intents
+client = discord.Client(intents = intents)
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    try:
+        chat_completion = openai_client.chat.completions.create(
+            messages=[{"role": "user", "content": message.content[5:]}],
+            model="gpt-3.5-turbo",
+        )
+        await message.channel.send(chat_completion.choices[0].message.content)
+    except Exception as e:
+        await message.channel.send(f"An error occured: {e}")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+client.run(DISCORD_TOKEN)
